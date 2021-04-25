7a328c2daa7e8579d5178def5cafa9d1aa14823b
gardc <gard@cappelendlgital.no>
2021-04-25 06:13:09 +0200

    asdasd

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -1,14 +1,18 @@
 ﻿// CMakeProject1.cpp : Defines the entry point for the application.
 //
 
+#include <iostream>
 #include "CMakeProject1.h"
 #include "test.h"
 
-using namespace std;
+using std::endl;
 
 int main()
 {
-	Shit shit = {};
-	cout << "Hello CMake." << endl;
+	Shit shit;
+	std::cout << "Hello CMake, enter something to check if it's swag or not: ";
+	std::cin >> shit.someText;
+	std::cout << endl << endl;
+	DoTest(shit);
 	return 0;
 }
